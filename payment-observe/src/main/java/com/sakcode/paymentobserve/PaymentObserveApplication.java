f35893c072da6f561dc59cd2dac47e050b9ec7aa
Samreach Yan <samreachyan@gmail.com>
2025-06-05 14:56:41 +0700

    tool

--- payment-observe/src/main/java/com/sakcode/paymentobserve/PaymentObserveApplication.java
+++ payment-observe/src/main/java/com/sakcode/paymentobserve/PaymentObserveApplication.java
@@ -7,10 +7,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PaymentObserveApplication {
-	@Bean
-	MeterRegistryCustomizer metricsCommonTags() {
-		return registry -> registry.config().commonTags("application", "Payment-Service");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentObserveApplication.class, args);
